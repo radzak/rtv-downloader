c9552bd8f7e929aee0fdc415216126dc4cfab744
Radek Krzak <radek.krzak@gmail.com>
2017-12-09 13:46:30 +0100

    Fix vodtvp, scraping description, title, show name and date now works

--- rtv/downloader/vodtvp.py
+++ rtv/downloader/vodtvp.py
@@ -1,38 +1,91 @@
 import datetime
+import json
 import re
+
+from bs4 import BeautifulSoup
 
 from rtv.downloader.common import Downloader
 
 
 class VodTVPDL(Downloader):
-    _VALID_URL = r'https?://(?:www\.)?vod.tvp\.pl/.*/(?P<show_name>[\w-]+),(?:[\w-]+-)?(?P<date>\d{8}).*'
+    _VALID_URL = r'https?://(?:www\.)?vod.tvp\.pl/' \
+                 r'.*?' \
+                 r'(?:,?(?P<date>[\d\-]+)?)' \
+                 r',' \
+                 r'(?P<object_id>\d+)'
 
     def get_podcast_date(self):
         match = re.match(self._VALID_URL, self.url)
+        if not match:
+            return None
 
-        if match:
-            date_str = match.group('date')
-            return datetime.datetime.strptime(date_str, '%d%m%Y')
+        date_str = match.group('date')
+        date_formats = [
+            '%d%m%Y',
+            '%d%m%Y-%H%M'
+        ]
+        for d in date_formats:
+            try:
+                return datetime.datetime.strptime(date_str, d)
+            except (ValueError, AttributeError):
+                pass
 
-    def get_show_name(self):
-        show_name_raw = super().get_info().get('title')
-        match = re.match(r'(?P<show_name>.*),(?:.*,)?\s*\d{2}.\d{2}.\d{4}', show_name_raw)
+    def get_podcast_show_name(self):
+        """
+        Get podcast show name from the podcast site. It's located in the div with 'data-hover'
+        attribute under the 'title' key.
+        Returns:
+            str: Podcast show name.
 
-        if match:
-            return match.group('show_name')
+        """
+        soup = BeautifulSoup(self.html, 'html.parser')
+        div = soup.find('div', attrs={'data-hover': True})
+        data = json.loads(div['data-hover'])
+        show_name = data.get('title')
 
-    # TODO: FIX invoking super().get_info() two times, add it as instance attribute and set in init?
-    def get_title(self):
-        return self.get_show_name()  # These shows have no title, only show_name and description
+        return show_name
 
-    # TODO: Scrape title from url and capitalize letters (or from webpage)
+    def get_podcast_title(self):
+        """
+        Get podcast title from the podcast site. It's located in the div with 'data-hover'
+        attribute under the 'episodeCount' key.
+        Returns:
+            str: Podcast title.
+
+        """
+        # considered as a worse solution since most of the podcasts have only date in the title
+        # soup = BeautifulSoup(self.html, 'html.parser')
+        # div = soup.find('div', attrs={'data-hover': True})
+        # data = json.loads(div['data-hover'])
+        # title = data.get('episodeCount')
+
+        soup = BeautifulSoup(self.html, 'html.parser')
+        title = soup.find('meta', {'property': 'og:title'})['content']
+        return title
+
+    def get_podcast_description(self):
+        """
+        Get podcast description from the podcast site. It's located in the meta tag
+        with 'og:description' attribute under 'content' attribute.
+        Returns:
+            str: Podcast description.
+
+        """
+        soup = BeautifulSoup(self.html, 'html.parser')
+        description = soup.find('meta', {'property': 'og:description'})['content']
+        return description
 
     def get_info(self):
-        entry = {
-            'title': self.get_title(),
-            'show_name': self.get_show_name(),
-            'date': self.get_podcast_date(),
-            'url': self.url,
-            'ext': 'mp4'
+        self.get_html()
+
+        podcast_info = {
+            'entries': [{
+                'title': self.get_podcast_title(),
+                'show_name': self.get_podcast_show_name(),
+                'description': self.get_podcast_description(),
+                'date': self.get_podcast_date(),
+                'url': self.url,
+                'ext': 'mp4'
+            }]
         }
-        return {'entries': [entry]}
+        return podcast_info
