8963c2a489e18419ec4e5cf9dbfab8e04a92da88
Radek Krzak <radek.krzak@gmail.com>
2018-01-17 21:32:55 +0100

    Add version to __init__.py, add pytest handling to setup.py
    Now you can run tests with "python setup.py test".

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from setuptools import setup
 
+import rtv
+
 DESCRIPTION = 'RTV podcast downloader'
 LONG_DESCRIPTION = 'Command-line program to download podcasts from various sites.'
 
 setup(name='rtv',
-      version='0.1',
+      version=rtv.__version__,
       description=DESCRIPTION,
       long_description=LONG_DESCRIPTION,
-      url='https://github.com/radzak/RTVdownloader',
+      url='https://github.com/radzak/RtvDownloader',
       author='Radek Krzak',
       author_email='radek.krzak@gmail.com',
       license='MIT',
-      packages=['rtv', 'rtv.downloader'],
+      packages=['rtv', 'rtv.downloader', 'rtv.extractor'],
       entry_points={
-          'console_scripts': ['rtv=rtv.command_line:main'],
+          'console_scripts': ['rtv=rtv.cli:main'],
       },
       install_requires=[
           'requests',
@@ -23,4 +25,6 @@ setup(name='rtv',
           'validators',
           'beautifulsoup4'
       ],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
       zip_safe=False)
