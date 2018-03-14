6531b63cbcbcfc070115c5a70b23e0f7b028d831
Radek Krzak <radek.krzak@gmail.com>
2018-03-14 16:31:07 +0100

    Refactor setup.py + add tldextract to required packages

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 import rtv
 
-DESCRIPTION = 'RTV podcast downloader'
-LONG_DESCRIPTION = 'Command-line program to download podcasts from various sites.'
+DESCRIPTION = 'RTV downloader'
+LONG_DESCRIPTION = 'Command-line program to download videos from various sites.'
 
 setup(name='rtv',
       version=rtv.__version__,
       description=DESCRIPTION,
       long_description=LONG_DESCRIPTION,
-      url='https://github.com/radzak/RtvDownloader',
+      url='https://github.com/radzak/RTVdownloader',
       author='Radek Krzak',
       author_email='radek.krzak@gmail.com',
       license='MIT',
@@ -23,7 +23,8 @@ setup(name='rtv',
           'Js2Py',
           'youtube_dl',
           'validators',
-          'beautifulsoup4'
+          'beautifulsoup4',
+          'tldextract'
       ],
       setup_requires=['pytest-runner'],
       tests_require=['pytest'],
