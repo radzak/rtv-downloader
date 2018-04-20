0b772391c6ee4b95d6a53d92b6a698a964195698
radzak <radek.krzak@gmail.com>
2018-04-20 15:01:02 +0200

    Use descriptors in Video implementation

--- rtv/video.py
+++ rtv/video.py
@@ -4,7 +4,34 @@ from rtv.downloaders.youtubedl import YoutubePD
 from rtv.utils import clean_video_data
 
 
-class Video:
+class Field:
+    def __init__(self):
+        self.name = None
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return instance.data.get(self.name)
+
+    def __set__(self, instance, value):
+        raise AttributeError(f"You can't set {self.name} attribute manually.")
+
+
+class Meta(type):
+    def __new__(mcs, name, bases, class_dict):
+        for key, value in class_dict.items():
+            if isinstance(value, Field):
+                value.name = key
+        cls = type.__new__(mcs, name, bases, class_dict)
+        return cls
+
+
+class Video(metaclass=Meta):
+    title = Field()
+    date = Field()
+    url = Field()
+    ext = Field()
+
     def __init__(self, data):
         self.data = clean_video_data(data)
 
@@ -12,25 +39,12 @@ class Video:
         ypd = YoutubePD(self, **kwargs)
         ypd.download()
 
-    @property
-    def title(self):
-        return self.data.get('title')
-
-    @property
-    def date(self):
-        return self.data.get('date')
-
-    @property
-    def url(self):
-        return self.data.get('url')
-
-    @property
-    def ext(self):
-        return self.data.get('ext')
-
     def print_data(self):
         """Pretty print the data of this video."""
         pprint.pprint(self.data)
 
     def __str__(self):
-        return f'Video - {self.title}'
+        return f'Video({self.title})'
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.data})'
