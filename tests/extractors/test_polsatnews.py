10283181f3e842f90b74315942ce682773fc3679
radzak <radek.krzak@gmail.com>
2018-09-22 12:55:07 +0200

    Remove not working test case url for PolsatNews

--- tests/extractors/test_polsatnews.py
+++ tests/extractors/test_polsatnews.py
@@ -6,6 +6,5 @@ from ..test_extractors import ExtractorTester
 class TestPolsatNewsExtractor(ExtractorTester):
     extractor_class = PolsatNews
     urls = [
-        'http://www.polsatnews.pl/wideo/nie-zyje-chlopiec-pod-ktorym-zalamal-sie-lod-w-opolskiem_6550622/?ref=wideo_top_kraj',
         'http://www.polsatnews.pl/wideo-program/prezydenci-i-premierzy-prawo-i-sprawiedliwosc-rusza-w-polske_6710058/'
     ]
