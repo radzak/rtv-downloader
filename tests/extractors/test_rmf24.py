9d2fe723db248d3ea90d137e7bbddb9248534234
radzak <radek.krzak@gmail.com>
2018-09-22 12:23:04 +0200

    Add test case urls for polsatnews and rmf24

--- tests/extractors/test_rmf24.py
+++ tests/extractors/test_rmf24.py
@@ -6,5 +6,6 @@ from ..test_extractors import ExtractorTester
 class TestRmf24Extractor(ExtractorTester):
     extractor_class = Rmf24
     urls = [
-        'http://www.rmf24.pl/tylko-w-rmf24/rozmowa/news-abp-gadecki-o-rozancu-do-granic-przygotowane-nie-przez-ksiez,nId,2449655'
+        'http://www.rmf24.pl/tylko-w-rmf24/rozmowa/news-abp-gadecki-o-rozancu-do-granic-przygotowane-nie-przez-ksiez,nId,2449655',
+        'https://www.rmf24.pl/tylko-w-rmf24/rozmowa-poludnie/news-pawel-mucha-o-zmianie-terminu-referendum-nie-ma-takiej-mozli,nId,2610035'
     ]
