113b9505fa048e69d245c5caf873cf8cc4afd845
radzak <radek.krzak@gmail.com>
2018-09-23 19:14:51 +0200

    Add ipla test case url

--- tests/extractors/test_ipla.py
+++ tests/extractors/test_ipla.py
@@ -6,6 +6,6 @@ from ..test_extractors import ExtractorTester
 class TestIplaExtractor(ExtractorTester):
     extractor_class = Ipla
     urls = [
-        'https://www.ipla.tv/wideo/news/Gosc-Wydarzen/5007380/'
-        'Gosc-Wydarzen-Pawel-Adamowicz/090c6a4705c443633df966d648040a8a'
+        'https://www.ipla.tv/wideo/news/Gosc-Wydarzen/5007380/Gosc-Wydarzen-Pawel-Adamowicz/090c6a4705c443633df966d648040a8a',
+        'https://www.ipla.tv/wideo/news/Rozmowa-dnia/5000237/Rozmowa-dnia-prof-Jan-Zielonka/2f90e244a713e9bcce9eabe4bd6105c3'
     ]
