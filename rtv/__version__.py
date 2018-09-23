540b07d8dd86dd9edbc04d414d8199af0433b539
radzak <radek.krzak@gmail.com>
2018-09-23 19:14:56 +0200

    Bump version to 2.0.6

--- rtv/__version__.py
+++ rtv/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'rtv-downloader'
 __description__ = 'Command-line program to download videos from various sites.'
 __url__ = 'https://github.com/radzak/rtv-downloader'
-__version__ = '2.0.5'
+__version__ = '2.0.6'
 __author__ = 'Radek Krzak'
 __author_email__ = 'radek.krzak@gmail.com'
 __license__ = 'MIT'
